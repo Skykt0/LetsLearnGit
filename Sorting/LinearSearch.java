a9a1217804b9fe146fb9e8946eaa44967bdd30d5
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-10-12 09:15:35 +0530

    added the linear search algo

--- Sorting/LinearSearch.java
+++ Sorting/LinearSearch.java
@@ -1,5 +1,16 @@
 package Sorting;
 
 public class LinearSearch {
-    
+    public static void main(String[] args){
+        int[] a={1,2,3,4,5,6,7};
+        int target = 4;
+        int ans = 0;
+        for(int i=0; i<a.length; i++){
+            if(a[i]==target){
+                ans = i;
+                break;
+            }
+        }
+        System.out.println("target element found at "+ans+" index");
+    }
 }
